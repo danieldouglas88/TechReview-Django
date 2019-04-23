df0654c6277e33cf2994c5a206914d652ae7ee75
Daniel Douglas <Daniel.Douglas@seattlecolleges.edu>
2019-04-22 18:36:58 -0700

    Commit #2

--- techreviewproject/techreviewproject/settings.py
+++ techreviewproject/techreviewproject/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'techreviewproject.urls'
+ROOT_URLCONF = 'techreviewapplication.urls'
 
 TEMPLATES = [
     {
